548e439e828aaf14faabd4269b9bc3ab47206d16
Glydewell Burdick <glydewellb@gmail.com>
2022-09-22 18:30:33 -0500

    add beginning monitoring alert

--- fridge_monitor.py
+++ fridge_monitor.py
@@ -10,9 +10,18 @@ class IFTTTService:
     def __init__(self, url):
         self.url = url
     
-    async def post(self, message, report: GoveeReading):
+    async def post_reading(self, message, report: GoveeReading):
         temp_C, humidity, battery = report.readings()
-        body = '{"value1":{},"value2":{},"value3":{}}"'.format(message, temp_C, battery)
+        body = "\"value1\":{},\"value2\":{},\"value3\":{}\"".format(message, temp_C, battery)
+        params =  "{" + body + "}"
+        await self.post_to_ifttt(params)
+
+    async def post_notice(self, message):
+        body = "\"value1\":{},\"value2\":\"n/a\",\"value3\":\"n/a\"\"".format(message)
+        params =  "{" + body + "}"
+        await self.post_to_ifttt(params)
+
+    async def post_to_ifttt(self, body):
         await requests.post(self.url, params=body)
 
 web_key = os.environ.get('IFTTT_KEY')
@@ -22,7 +31,7 @@ if web_key == None:
     print("IFTTT_KEY environment variable not set!")
     sys.exit(1)
 
-
+# Static values
 SENSOR_NAME =  "GVH5101_7F32"
 EVENT_NAME = "fridge_alert"
 IFTTT_URL = "https://maker.ifttt.com/trigger/{}/with/key/{}".format(EVENT_NAME, web_key)
@@ -30,17 +39,19 @@ IFTTT_URL = "https://maker.ifttt.com/trigger/{}/with/key/{}".format(EVENT_NAME, 
 scanner = SensorScanner(30.0)
 reporter = IFTTTService(IFTTT_URL)
 
+# Announce opening of monitoring
+reporter.post_notice('Beginning fridge monitoring')
+
 while True:
     reports = asyncio.run_until_complete(scanner.scan()) 
     # for now, just loop until you find the right reading
-    for report in reports:
+    for report in reports.values():
         if report.name == SENSOR_NAME:
             
             if report.temp_F() > 32.1:
-                reporter.post('Fridge too warm!', report)
+                reporter.post_reading('Fridge too warm!', report)
             
             elif report.battery() < 20:
                 reporter.post('Sensor Battery Failing!', report)
             
-        # 
     scanner.clear_readings()
