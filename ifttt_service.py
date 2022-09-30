bde48b6122144364c3814b60a7cfeb43633557dc
Glydewell Burdick <glydewellb@gmail.com>
2022-09-30 11:32:38 -0500

    Pull ifttt service into own module

---
+++ ifttt_service.py
@@ -0,0 +1,17 @@
+import requests
+import os
+import sys
+from sensors.GoveeSensor import GoveeReading
+
+class IFTTTService:
+    def __init__(self, event: str, key: str):
+        self.url = "https://maker.ifttt.com/trigger/{}/with/key/{}".format(event, key)
+
+    async def post(self, message: str, report: GoveeReading = None):
+        temp_text, batt_text = "n/a", "n/a" # default values
+        if report != None:
+            temp_text = f'{report.temp_F()} deg F'
+            batt_text = f'{report.battery()}%'
+        body = {"value1": message,"value2": temp_text, "value3": batt_text}
+        await requests.post(self.url, params=body)
+        pass
